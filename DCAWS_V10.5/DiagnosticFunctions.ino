4ed9c207f24c832482d137431c7ae70c3226f5d0
alexandreabarker <48071916+alexandreabarker@users.noreply.github.com>
2019-03-18 18:18:46 -0400

    Add files via upload

---
+++ DCAWS_V10.5/DiagnosticFunctions.ino
@@ -0,0 +1,59 @@
+void systemDiagnosis()
+{
+  radio.println(F("Running System Diagnostic"));
+  delay(SETUP_DELAY);
+  checkRadio();
+  checkGPS();
+  checkSD();
+  checkThruster();
+  getDepth();
+  radio.print(F("Depth sensor detecting depth of "));
+  radio.println(depth);
+  getInternalCurrent();
+  radio.print(F("Internal current sensor detecting current of "));
+  radio.println(current);
+  if(!getInternalCurrent())
+  {
+    radio.print(F("Internal current is below acceptable value!")); 
+    missionReady = false;
+  }
+  getInternalTemp();
+  radio.print(F("Internal temperature sensor detecting temperature of "));
+  radio.println(temperature);
+  if(!getInternalTemp())
+  { 
+    radio.print(F("Internal temperature is above acceptable value!"));
+    missionReady = false;
+  }
+  getInternalPressure();
+  radio.print(F("Internal pressure sensor detecting pressure of "));
+  radio.println(pressure);
+  if(!getInternalPressure())
+  {
+    radio.print(F("Internal pressure is above acceptable value!"));
+    missionReady = false;
+  }
+  getInternalLeak();
+  radio.print(F("Internal leak sensor detecting voltage of "));
+  radio.println(leakVoltage);
+  if(getInternalLeak())
+  {
+    radio.print(F("No leak detected."));
+  }
+  else
+  {
+    radio.print(F("Leak detected!"));
+    missionReady = false;
+  }
+  if(missionReady)
+  {
+    radio.println(F("No issues detected. Deployment recomended.")); 
+  }
+  else
+  {
+    radio.println(F("Issue(s) detected! Deployment not recomended."));
+    radio.println(F("Please review error messages and resolve issue before proceeding."));
+    
+  }
+}
+
---
+++ DCAWS_V10.5/SDFunctions.ino
@@ -0,0 +1,36 @@
+void setupSD()
+{
+  //Initialize the SD Card Slot
+  radio.println(F("Initializing SD card..."));
+  pinMode(chipSelect, OUTPUT); //For the SD Card to work
+  if (!SD.begin(chipSelect))
+  {
+    radio.println(F("initialization failed!"));
+    return;
+  }
+  dcawsLog = SD.open("dcawsLog.csv", FILE_WRITE);
+}
+
+void checkSD()
+{
+  if (!SD.begin(chipSelect))
+  {
+    radio.println(F("SD card initialization failed!"));
+    missionReady = false;
+    return;
+  }
+  radio.print(F("SD card initialization confirmed and found files: "));
+  if (dcawsLog)
+    radio.print(F("dcawsLog, "));
+}
+
+
+void logData()
+{
+  if(dcawsLog)
+  {
+    //File dcawsLog = SD.open("dcawsLog.csv", FILE_WRITE);  
+    dcawsLog.println(dataString);
+    dcawsLog.flush();  
+  }
+}
